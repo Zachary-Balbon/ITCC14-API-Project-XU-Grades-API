d8cd036cbc6c3c530101c3262126a1b69e107638
Evan Simbajon <200720255@my.xu.edu.ph>
2025-11-06 20:10:30 +0800

    refactor: Comment added for clarity

--- api.py
+++ api.py
@@ -9,6 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 api = Api(app)
 
+#Models for each resource
 class StudentModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     student_name = db.Column(db.String(255), nullable=False)
